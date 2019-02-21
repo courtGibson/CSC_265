1d0cb571ca7cd4fdefe91d3c11a5d1db4b1a99b6
courtGibson <47224669+courtGibson@users.noreply.github.com>
2019-02-20 20:00:01 -0500

    Create calcJS.js

---
+++ calcJS.js
@@ -0,0 +1,13 @@
+
+var nums = [11, 4, 41, 50, 7, 19, 8, 18, 2, 6, 43, 6, 20, 43, 2, 23, 12, 24, 31, 29];
+var total = 0;
+var acc = 0;
+nums.forEach(function(d){total=total+d; acc++;});
+
+var mean = function(total, acc)
+{
+  return total/acc;
+}
+
+
+document.write(mean)
